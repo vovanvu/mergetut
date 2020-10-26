cce02600347d0d9e0db4a5d765fbd5fc07e4745e
Võ Văn Vũ <vanvu2912@gmail.com>
2020-10-26 11:54:53 +0700

    Update logsomething.js

--- logsomething.js
+++ logsomething.js
@@ -5,7 +5,3 @@ logHello1();
 <<<<<<< HEAD
 console.log("aaaaabbbbcccc");
 console.log("dddddd");
-console.log("111111");
-=======
-//add some comments
->>>>>>> 8e0a90e3bdb7f7e92e4c2fb16aad850493723296
